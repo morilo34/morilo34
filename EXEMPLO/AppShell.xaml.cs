c01e3df93d03358ad9f224db4733dad3cd574d46
morilo34 <muriloap34@gmail.com>
2024-03-06 18:19:36 -0300

    chore:adiciona enviar mensagem

---
+++ EXEMPLO/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EXEMPLO
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EXEMPLO/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace EXEMPLO
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)  
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+        string name = "";
+        name = NameEntry.Text;
+        MessageLabel.Text = $"Hello, {name}!";
+        }
+
+
+
+}
